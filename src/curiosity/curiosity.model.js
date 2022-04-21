9ba7fb087da77a7147f5a1e9079807232885e729
macaeco <macarenafernandezayusob@gmail.com>
2022-04-21 16:18:57 +0200

    commit pre despliegue proyecto finiquitao

--- src/curiosity/curiosity.model.js
+++ src/curiosity/curiosity.model.js
@@ -2,8 +2,8 @@ import { MongoClient } from 'mongodb';
 const URI = 'mongodb+srv://maceco:Mz3rys85ANhCrw9@dfl-cluster.eqcv7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const client = new MongoClient(URI);
 const DATABASE_NAME = 'dfl-macaecobd';
-const COLLECTION_NAME = 'listsFilms';
-export const createList = async (list) => {
+const COLLECTION_NAME = 'curiosity';
+export const createCur = async (cur) => {
     try{
         await client.connect(); // paso 6
         const db = client.db(DATABASE_NAME); // paso 7
@@ -16,18 +16,18 @@ export const createList = async (list) => {
         await client.close(); // paso 10. Cerramos la conexión
     }
 }
-export const retrieveLists = async () => {
+export const retrieveCurs = async () => {
     try{
         await client.connect(); // paso 6
         const db = client.db(DATABASE_NAME); // paso 7
-        const listsCol = db.collection(COLLECTION_NAME); // paso 8
+        const cursCol = db.collection(COLLECTION_NAME); // paso 8
         const opt = {
             projection: { _id:0 }
         }
         // a partir de aqui ya puedo hacer operaciones con la collection
-        const lists = await listsCol.find({}, opt).toArray(); // paso 9 devuelve todos los documentos en formato Array de JS
-        console.log(lists)
-        return lists;
+        const curs = await cursCol.find({}, opt).toArray(); // paso 9 devuelve todos los documentos en formato Array de JS
+        console.log(curs)
+        return curs;
     }catch(err){
         console.error('error: ', err);
     }finally {
